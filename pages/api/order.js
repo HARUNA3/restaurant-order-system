ceef14987ff4a691bc798238427ff276394c9b45
Haruna Fujishima <ringo144516@gmail.com>
2025-10-24 05:33:55 -0400

    modify code

---
+++ pages/api/order.js
@@ -0,0 +1,12 @@
+export default function handler(req, res){
+    if(req.method === "POST"){
+        const order = req.body;
+        console.log("New Order:", order);
+        res.status(200).json({status: "OK"});
+
+    }else {
+        res.status(405).json({error: "Method not allowed"});
+    }
+}
+
+
